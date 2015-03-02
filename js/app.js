e7d08e722737a3b4e3152bacc2ff373d41732bba
Jerson Carranza Lucas <jerson_carranza2@hotmail.com>
2015-03-02 16:44:02 -0500

    botones

--- js/app.js
+++ js/app.js
@@ -18,4 +18,23 @@
 		this.tab = tab;
 		};
 	});
+
+	app.controller('CommentsController', function () {
+		this.comments = [];
+		this.comment = {};
+		this.show = false;
+		this.toggle = function () {
+		this.show = !this.show;
+		};
+		this.anonymousChanged = function () {
+		if (this.comment.anonymous) {
+			this.comment.email = "";
+			}
+		};
+		this.addComment = function () {
+			this.comment.date = Date.now();
+			this.comments.push(this.comment);
+			this.comment = {};
+		};
+	});
 })();
