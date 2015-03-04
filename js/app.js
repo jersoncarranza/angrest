118a392c63535cd3eda1be99a41fc557efeb9912
Jerson Carranza Lucas <jerson_carranza2@hotmail.com>
2015-03-03 20:18:38 -0500

    paso

--- js/app.js
+++ js/app.js
@@ -1,40 +1,9 @@
 (function () {
 	
-	var app = angular.module("MyApp", []);
+	var app = angular.module("MyApp", [
+		'controllers'
+		]);
 
-	app.controller("PostsCtrl", function($scope, $http) {
-	  $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
-	  console.log('*');
-	  $http.get('http://127.0.0.1:7000/files/list/').
-	    success(function(data, status, headers, config) {
-	        $scope.posts = data;
-	        console.log(data);
-	    });
-	});
 
-	app.controller('TabsController', function () {
-	this.tab = 1;
-		this.selectTab = function (tab) {
-		this.tab = tab;
-		};
-	});
 
-	app.controller('CommentsController', function () {
-		this.comments = [];
-		this.comment = {};
-		this.show = false;
-		this.toggle = function () {
-		this.show = !this.show;
-		};
-		this.anonymousChanged = function () {
-		if (this.comment.anonymous) {
-			this.comment.email = "";
-			}
-		};
-		this.addComment = function () {
-			this.comment.date = Date.now();
-			this.comments.push(this.comment);
-			this.comment = {};
-		};
-	});
 })();
